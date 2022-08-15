e6b48729036ffb7659c0ef52345453f9d7874327
Michael Mattie <michael.mattie.employers@gmail.com>
2022-08-15 02:16:41 -0700

    major rewrite of grail-load.el fixing it and eliminating uneccesary code

--- emacs/grail-load.el
+++ emacs/grail-load.el
@@ -6,91 +6,76 @@
 ;; grail-match - return a list of paths that match a given criteria
 ;;
 
-(defun grail-match-path ( directory filters )
-  (let
-    ((contents (directory-files-and-attributes directory ))
-     (filtered nil))
-
-    (mapc (lambda ( path )
-            (catch 'eliminate
-              (mapc (lambda ( filter )
-                      (grail-match-path-logic path filter
-                        (throw 'eliminate t)))
-                filters)
-
-              (setq filtered (cons path filtered)) ))
-      contents)
-
-    (mapcar (lambda ( path-with-attributes )
-              (concat directory "/" (car path-with-attributes)) )
-      filtered) ))
-
-;;
-;; grail-match - return a list of paths that match a given criteria
-;;
-
 (defun grail-elisp-files-only ( path-arg )
   (directory-files path-arg t ".*\.elc?$"))
 
 (defun grail-subdirs-only ( path-arg &optional test )
   (let
-    ((dirs '()))   
-     
+    ((dirs '()))
+
     (mapc
-     (lambda ( path )
-       (let
-	   ((name (file-name-base path)))
+      (lambda ( path )
+        (let
+          ((name (file-name-base path)))
 
-	 (when (and (not (string-equal "." name))
-		    (not (string-equal ".." name))
-		    (file-directory-p path)
-		    (if test (funcall test path) t))
-           (setq dirs (cons path dirs))) ))
+          (when (and (not (string-equal "." name))
+                     (not (string-equal ".." name))
+                     (file-directory-p path)
+                  (if test (funcall test path) t))
+            (setq dirs (cons path dirs))) ))
 
-       (directory-files path-arg t))
+      (directory-files path-arg t))
 
-      dirs))
+    dirs))
 
 (defun grail-dirs-recurse ( dir &optional test )
   (if (and dir (file-directory-p dir))
-      (cons dir (mapcar 'grail-dirs-recurse (grail-subdirs-only dir test)))
+      (apply 'append (list dir) (mapcar 'grail-dirs-recurse (grail-subdirs-only dir test)))
     '() ))
-
 
 (defun grail-new-load-path ( &rest libraries )
   (let
       ((elisp-dirs '())
-       (lib-list  nil))
+       (lib-list  nil)
+       (verified '()))
 
     (mapc
        (lambda ( lib-arg )
-	 (if (listp lib-arg)
-	   (setq lib-list (append lib-arg lib-list))
-	   (setq lib-list (cons lib-arg lib-list)) ))
-       libraries)
-      
+         (if (listp lib-arg)
+           (setq lib-list (append lib-arg lib-list))
+           (setq lib-list (cons lib-arg lib-list)) ))
+      libraries)
+
+    (mapc
+      (lambda ( lib-arg )
+       (when (file-directory-p lib-arg)
+         (setq verified (cons lib-arg verified))) )
+      lib-list)
+
     (mapc
      (lambda ( lib-dir )
        (progn
-	 (when (grail-elisp-files-only lib-dir)
-	   (setq elisp-dirs (cons lib-dir elisp-dirs)))
+         (when (grail-elisp-files-only lib-dir)
+           (setq elisp-dirs (cons lib-dir elisp-dirs)))
 
-	 (let
-	     ((sub-dirs (mapcar (lambda ( nested-dir )
-				  (grail-dirs-recurse nested-dir 'grail-elisp-files-only))
-				  (grail-subdirs-only lib-dir)) ))
-	   (setq elisp-dirs (append sub-dirs elisp-dirs)) )))
-     lib-list)
+         (let
+           ((sub-dirs (apply 'append
+                        (mapcar
+                          (lambda ( nested-dir )
+                            (grail-dirs-recurse nested-dir 'grail-elisp-files-only))
+                          (grail-subdirs-only lib-dir)) )))
+           (setq elisp-dirs (append sub-dirs elisp-dirs)) )))
+      verified)
 
     (let
-	((final-path-list elisp-dirs))
+      ((final-path-list elisp-dirs))
 
       (when grail-platform-load-path
-	(setq final-path-list (append grail-platform-load-path elisp-dirs)))
+        (setq final-path-list (append grail-platform-load-path elisp-dirs)))
 
       (setq load-path final-path-list))
 
-    lib-list))
+    verified))
 
 ;;
 ;; keep a table of all the dirs where we install so we can later
@@ -108,13 +93,15 @@
     library-list))
 
 (defun grail-update-library-files ( library-dir )
-  (mapc
-   (lambda ( elisp-file )
-      (unless (gethash (file-name-sans-extension elisp-file) grail-library-table)
-        (puthash (file-name-sans-extension elisp-file) elisp-file grail-library-table)))
-   (grail-elisp-files-only library-dir)) )
+  (when (file-directory-p library-dir)
+    (mapc
+     (lambda ( elisp-file )
+       (unless (gethash (file-name-sans-extension elisp-file) grail-library-table)
+         (puthash (file-name-sans-extension elisp-file) elisp-file grail-library-table)))
+     (grail-elisp-files-only library-dir)) ))
 
 (defun grail-update-load-path ()
+  (interactive)
   "grail-update-load-path
 
    update the load path. all of the paths listed are scanned for sub-dirs and
@@ -123,28 +110,27 @@
   (let
     ((libraries
       (grail-new-load-path
-	;;
-	;; my code smells like roses so load first.
-	;;
-	grail-local-elisp
+        ;;
+        ;; my code smells like roses so load first.
+        ;;
+        grail-local-elisp
 
-	;;
-	;; fresh elisp
-	;;
+        ;;
+        ;; fresh elisp
+        ;;
+        grail-dist-elisp
 
-	grail-dist-elisp
 
+        ;;
+        ;; fresh version control
+        ;;
+        grail-dist-git
 
-	;;
-	;; fresh version control
-	;;
-	grail-dist-git
+        ;;
+        ;; ELPA kinda stale
+        ;;
+        grail-elpa-load-path)))
 
-	;;
-	;; ELPA kinda stale
-	;;
-	grail-elpa-load-path)))
-    
     (grail-update-library-dirs libraries)
 
     (grail-update-library-files grail-local-elisp)
