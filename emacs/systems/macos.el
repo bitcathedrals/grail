cf445baf38f108db1ca30b7bf8546b42a67c9dca
Michael Mattie <codermattie@runbox.com>
2022-09-01 13:12:56 -0700

    uniquify the exec-path list

--- emacs/systems/macos.el
+++ emacs/systems/macos.el
@@ -11,12 +11,12 @@
 (setq grail-font-size 160)
 
 (let
-  ((user-brew-path (concat (getenv "HOME") "/homebrew/bin/")))
+  ((user-brew-path (concat (getenv "HOME") "/homebrew/bin")))
 
   (if (file-exists-p (concat user-brew-path "brew"))
     (setq exec-path (cons user-brew-path exec-path))) )
 
-(setq exec-path (cons "/usr/local/bin" exec-path))
+(setq exec-path (seq-uniq (cons "/usr/local/bin" exec-path)))
 
 ;; for some reason when I run emacs out of the dock
 ;; /usr/local/bin is missing.
