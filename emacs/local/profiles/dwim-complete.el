212bff897f7cfff215d1105e28d44d9005b0661d
Michael Mattie <codermattie@runbox.com>
2022-09-03 17:34:28 -0700

    first draft of a new dwim-complete

--- emacs/local/profiles/dwim-complete.el
+++ emacs/local/profiles/dwim-complete.el
@@ -8,35 +8,28 @@
 ;; currently helm is outstanding for this purpose.
 ;;----------------------------------------------------------------------
 (require 'thingatpt)
-
+(require 'subr-x)
 (require 'helm)
+
+;;
+;; options
+;;
 
 (setq helm-execute-action-at-once-if-one t)
 
-(defun dwim-complete/make-name ( name )
-  `(name . ,name))
+(defvar dwim-complete-fuzzy-match t)
 
-(defun dwim-complete/make-candidates ( candidates-fn )
-  `(candidates .
-     (lambda ()
-       (sort (mapcar (lambda (x) (identity x)) (,candidates-fn)) 'string-lessp)) ))
+;;
+;; interface to helm
+;;
 
-(defun dwim-complete/make-action ( &optional fn )
-  `(action . ,(if fn fn (lambda (selection) selection))))
-
-(defun dwim-complete/make-source ( name candidates-fn action )
-  `(,(dwim-complete/make-name name)
-    ,(dwim-complete/make-candidates candidates-fn)
-    ,(dwim-complete/make-action action)))
-
-(defun dwim-complete/helm ( prompt input buffer &rest sources-list )
+(defun dwim-complete/helm ( prompt input helm-buffer helm-source )
   (helm
-    :sources sources-list
+    :sources helm-source
     :input input
     :prompt prompt
-    :buffer buffer))
+    :buffer helm-buffer))
 
-(defvar dwim-complete-local-sources nil)
 
 (defvar dwim-complete-stem-start nil)
 (defvar dwim-complete-stem-stop nil)
@@ -63,43 +56,13 @@
     (delete-region dwim-complete-stem-start dwim-complete-stem-stop)
     (goto-char dwim-complete-stem-start)) )
 
-(defvar dwim-complete-mode-sources (make-hash-table))
-(defvar dwim-complete-mode-types (make-hash-table))
-
-(defun dwim-complete-mode-add-source ( mode-name source )
-  (let
-    (( mode-source-list (gethash mode-name dwim-complete-mode-sources) ))
-
-    (setq mode-source-list (cons source mode-source-list))
-
-    (puthash mode-name mode-source-list dwim-complete-mode-sources) ))
-
-(defun dwim-complete-mode-get-sources ( mode-name )
-  (gethash mode-name dwim-complete-mode-sources))
-
-(defun dwim-complete-mode-add-type ( mode-name type )
-  (let
-    (( type-list (gethash mode-name dwim-complete-mode-types) ))
-
-    (setq type-list (cons type type-list))
-
-    (puthash mode-name type-list dwim-complete-mode-types) ))
-
-(defun dwim-complete-mode-check-type ( mode-name type )
-  (let
-    (( type-list (gethash mode-name dwim-complete-mode-types) ))
-
-    (if type-list
-      (member type type-list)
-      nil) ))
-
 (defun dwim-complete-behind-point ()
   (interactive)
 
   (let
-    (( stem (thing-at-point 'symbol) )
-     ( start nil )
-     ( end (point)))
+    ((stem (thing-at-point 'symbol) )
+     (start nil )
+     (end (point)))
 
     (if stem
       (progn
@@ -107,32 +70,117 @@
         stem)
       "") ))
 
+;;
+;; dwim-complete source data type
+;;
+
+(defun dwim-complete/make-name (name)
+  `("name" . ,name))
+
+(defun dwim-complete/make-completions ( completions-fn )
+  `("candidates" . completions-fn))
+
+(defun dwim-complete/make-action ( &optional fn )
+  `("action" . ,(if fn fn (lambda (selection) selection))))
+
+(defun dwim-complete/make-source ( name completions-fn action )
+  (list
+    (dwim-complete/make-name name)
+    (dwim-complete/make-completions completions-fn)
+    (dwim-complete/make-action action)))
+
+;;
+;; accessor functions
+;;
+
+(defun dwim-complete-get-name (source)
+  (assoc "name" source))
+
+(defun dwim-complete-get-completions (source)
+  (assoc "candidates" source))
+
+(defun dwim-complete-get-action (source)
+  (assoc "action" source))
+
+;;
+;; dwim-complete sources data table
+;;
+
+(defvar dwim-complete-mode-sources nil "Global dwim-complete sources")
+(defvar-local dwim-complete-local-sources nil "local dwim-complete sources")
+
+(defun dwim-complete-mode-add-source ( mode-name source &optional local )
+  (if local
+    (setq dwim-complete-local-sources (cons source dwim-complete-local-sources))
+    (let
+      ((global (assoc mode-name dwim-complete-mode-sources)))
+
+      (if global
+        (setcdr global (cons source (cdr global)))
+        (setq dwim-complete-mode-sources
+          (cons `(,mode-name . ,(list source)) dwim-complete-mode-sources)) )) ))
+
+(defun dwim-complete-get-sources (mode-name select-field)
+  (let
+    ((merged-table (assoc mode-name dwim-complete-mode-sources))
+     (completers nil))
+
+    (when dwim-complete-local-sources
+      (setq merged-table (append dwim-complete-local-sources merged-table)))
+
+    (when merged-table
+      (mapcar
+        (lambda ( source )
+          (setq completers (cons (select-field source) completers)))
+        merged-table))
+
+    completers))
+
+;;
+;; build helm source from sources table
+;;
+
 (defun dwim-complete/buffer ()
   (get-buffer-create "*complete*"))
 
+(defun dwim-complete-build-candidates ()
+  (let
+    ((completers (dwim-complete-get-sources major-mode 'dwim-complete-get-completions)))
+    ((completions nil))
 
-;; a buffer local value for the mode to use for dwim. nil
-;; by default so the major-mode is the actual default.
-(defvar dwim-mode nil)
+    (when completers
+      (mapcar
+        (lambda ( complete )
+          (setq completions (append (complete) completions)))
+        completers)
 
-(defun dwim-complete/set-mode ( mode )
-  (set (make-local-variable 'dwim-mode) mode))
+      (sort completions 'string-lessp)) ))
 
-(defun dwim-complete/get-mode ()
-  (or dwim-mode major-mode))
+(defun dwim-build-helm-source ()
+  (let
+    ((candidates (dwim-complete-build-candidates)))
+
+    (when candidates
+      (helm-build-sync-source major-mode
+        :candidates (dwim-complete-build-candidates)
+        :fuzzy-match dwim-complete-fuzzy-match
+        :buffer (dwim-compete/buffer))) ))
+
 
 (defun dwim-complete/complete ()
+  "Attempt to perform a completion of what is behind the cursor."
   (interactive)
   (let
-    (( completions (dwim-complete-mode-get-sources (dwim-complete/get-mode)) ))
+    ((helm-source (dwim-build-helm-source)))
 
-    (if completions
-      (apply 'dwim-complete/helm
-        "complete: "
-        (dwim-complete-behind-point)
-        (dwim-complete/buffer)
-        completions)
-      (message "no completions available for mode: %s" (dwim-complete/get-mode)) ) ))
+    (if helm-source
+      (dwim-complete/helm
+        "complete: "                  ;; prompt
+        (dwim-complete-behind-point)  ;; input
+        (dwim-complete/buffer)        ;; buffer
+        helm-source                   ;; sources
+        )
+      (message "no completions available for mode: %s" (major-mode)) )))
 
 (defun dwim-complete/for-buffer ()
   (make-local-variable 'dwim-complete-stem-start)
