459ce637ea23339b9600a5dd1b29e0a8ed1e8212
Michael Mattie <michael.mattie.employers@gmail.com>
2022-09-14 14:11:08 -0700

    early draft of crypto support in emacs

--- emacs/crypto.el
+++ emacs/crypto.el
@@ -1,7 +1,14 @@
 ;;----------------------------------------------------------------------
-;; gnupg
+;; gnupg crypto support
 ;;----------------------------------------------------------------------
-(require 'epa-file)
-(epa-file-enable)
 
-(provide 'public-key)
+;; use pinentry-mac from homebrew on MacOS
+
+(require 'epg)
+(require 'epa-file)
+
+;; Enable loopback so that pinentry will pop up in emacs
+(setq
+  epg-user-id "codermattie@runbox.com")
+
+(provide 'crypto)
