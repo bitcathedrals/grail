695db8f52da463b7547524ec80bd0b602b5b2de6
Michael Mattie <codermattie@runbox.com>
2024-02-28 12:59:16 -0800

    (feat) revamp the scripts to build emacs from brew and deal with byte copmilation

---
+++ scripts/brew-emacs.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+brew tap d12frosted/emacs-plus
+brew install emacs-plus --with-native-comp
---
+++ scripts/install-emacs-app.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+osascript -e 'tell application "Finder" to make alias file to posix file "/usr/local/opt/emacs-plus@29/Emacs.app" at POSIX file "/Applications"'
