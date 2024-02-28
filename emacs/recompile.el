1e9881977e9a1cafb2e85d1ddb45035535d487a5
Michael Mattie <codermattie@runbox.com>
2024-02-28 09:35:57 -0800

    (feat) write a compile/recompile script to byte compile all the elisp files

---
+++ emacs/recompile.el
@@ -0,0 +1,33 @@
+(defun recompile-elisp-all ()
+  (interactive)
+
+  (mapcar
+    (lambda (dir)
+      (when (file-directory-p dir)
+        (message (concat "updating compiles in directory: " dir))
+
+        (grail-ignore
+          "recompile-elisp-all"
+          (format "re-compiling directory: %s" dir)
+
+          (byte-recompile-directory dir  t)) ))
+    load-path))
+
+(defun compile-elisp-all ()
+  (interactive)
+
+  (mapcar
+    (lambda (dir)
+      (when (file-directory-p dir)
+        (message (concat "compiling in directory: " dir))
+
+        (mapcar
+          (lambda (file)
+            (grail-ignore
+              "compile-elisp-all"
+              (format "compiling a elisp file: %s @ %s" file dir)
+
+              (byte-compile-file file)) )
+          (file-expand-wildcards (concat dir "/*.el")) )))
+    load-path))
+
