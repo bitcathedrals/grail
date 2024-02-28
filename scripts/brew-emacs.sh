23e23736359221dde9743ba31e7eac26582c981e
Michael Mattie <codermattie@runbox.com>
2024-02-28 15:18:16 -0800

    (fix) fix the emacs scripts to deal with a homebrew inside ~ situation

--- scripts/brew-emacs.sh
+++ scripts/brew-emacs.sh
@@ -2,3 +2,6 @@
 
 brew tap d12frosted/emacs-plus
 brew install emacs-plus --with-native-comp
+
+brew unlink emacs
+brew link emacs-plus@29
--- scripts/install-emacs-app.sh
+++ scripts/install-emacs-app.sh
@@ -1,3 +1,22 @@
 #! /usr/bin/env bash
 
-osascript -e 'tell application "Finder" to make alias file to posix file "/usr/local/opt/emacs-plus@29/Emacs.app" at POSIX file "/Applications"'
+app=""
+
+user_brew=/Users/mattie/homebrew/Cellar/emacs-plus@29/29.2/Emacs.app
+opt_brew=/usr/local/opt/emacs-plus@29/Emacs.app
+
+if [[ -d $user_brew ]]
+then
+   app=$user_brew
+   echo "using homedir brew: $user_brew"
+else
+  if [[ -d $opt_brew ]]
+  then
+    app=$opt_brew
+  else
+    echo "brew app could not be found in $user_brew or $opt_brew"
+    exit 1
+  fi
+fi
+
+osascript -e "tell application \"Finder\" to make alias file to posix file \"$app\" at POSIX file \"/Applications\""
