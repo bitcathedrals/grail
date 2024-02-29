11cf6fd21c51366a859a9553a24bb521c37b0a74
Michael Mattie <codermattie@runbox.com>
2024-02-29 07:43:33 -0800

    (feat) add the Cousine fonts

--- fonts/macos-install-for-user.sh
+++ fonts/macos-install-for-user.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
 
-test -e spleen || tar xf spleen.tar
-
 cp spleen/*.dfont ~/Library/Fonts/
 cp hack/*.ttf ~/Library/Fonts
 cp dejavu-sans-mono/*.ttf ~/Library/Fonts
+cp Cousine/*.ttf ~/Library/Fonts
 
 
