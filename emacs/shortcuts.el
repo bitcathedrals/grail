c79ef7319024d1003ba621f19695c6c02e21df38
Michael Mattie <codermattie@runbox.com>
2024-03-02 06:38:56 -0800

    (feat) add a goto common lisp

--- emacs/shortcuts.el
+++ emacs/shortcuts.el
@@ -2,6 +2,10 @@
   (interactive)
   (dired (concat (getenv "HOME") "/code/algorithms/src/scheme/sicp/")) )
 
+(defun goto-cl ()
+  (interactive)
+  (dired (concat (getenv "HOME") "/code/algorithms/src/cl/")) )
+
 (defun goto-emacs ()
   (interactive)
   (dired (concat (getenv "HOME") "/.emacs.grail/")) )
