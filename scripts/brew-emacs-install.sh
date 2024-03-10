e60a624d82e739e6c04edcd8a94511e355a982c2
Michael Mattie <codermattie@runbox.com>
2024-03-10 15:18:39 -0700

    (fix) fix finding the Emacs.app to create thet alias, make it more robust with a dynamic find

--- scripts/brew-emacs-install.sh
+++ scripts/brew-emacs-install.sh
@@ -2,21 +2,30 @@
 
 app=""
 
-user_brew=/Users/mattie/homebrew/Cellar/emacs-plus@29/29.2/Emacs.app
-opt_brew=/usr/local/opt/emacs-plus@29/Emacs.app
+home_brew=$(find /Users/mattie/homebrew -name 'Emacs.app' -print)
+user_brew=$(find /usr/local/opt/ -name 'Emacs.app' -print)
+opt_brew=$(find /opt/homebrew/ -name 'Emacs.app' -print)
 
-if [[ -d $user_brew ]]
+if [[ -e $home_brew ]]
 then
-   app=$user_brew
-   echo "using homedir brew: $user_brew"
-else
-  if [[ -d $opt_brew ]]
-  then
-    app=$opt_brew
-  else
-    echo "brew app could not be found in $user_brew or $opt_brew"
-    exit 1
-  fi
+   app=$home_brew
+fi
+
+if [[ -e $user_brew ]]
+then
+  app=$user_brew
+fi
+
+if [[ -e $opt_brew ]]
+then
+  app=$opt_brew
+fi
+
+
+if [[ -z $app ]]
+then
+  echo "brew app could not be found in $home_brew or $user_brew or $opt_brew"
+  exit 1
 fi
 
 osascript -e "tell application \"Finder\" to make alias file to posix file \"$app\" at POSIX file \"/Applications\""
