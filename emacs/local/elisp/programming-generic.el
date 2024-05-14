a226e5c777d629838fa1f0fb1ac74ae8997ff78b
Michael Mattie <codermattie@runbox.com>
2024-05-13 20:39:47 -0700

    (fix): turn on buffer-ring local keybindings in programming-generic [programming-generic.el]

--- emacs/local/elisp/programming-generic.el
+++ emacs/local/elisp/programming-generic.el
@@ -50,7 +50,6 @@
     (funcall programming-generic/buffer-functions)
     (message "no programming-generic/buffer-functions defined in buffer.")) )
 
-
 (defun programming-mode-generic ( &optional fn-search )
   "Enable my programming customizations for the buffer"
 
@@ -96,7 +95,9 @@
     ("a" . xref-find-apropos)
     ("w" . xref-find-definitions-other-window)
     ("p" . xref-go-back)
-    ("n" . xref-go-forward)) )
+    ("n" . xref-go-forward))
+
+  (buffer-ring/local-keybindings) )
 
 (defun get-clean-report-buffer ()
   (let
@@ -109,7 +110,17 @@
 (defun get-report-buffer ()
   (get-buffer-create "*report output*"))
 
-(defconst delta-conventional '("feat" "fix" "bug" "issue" "sync" "merge" "alpha" "beta" "release" "refactor" "doc"))
+(defconst delta-conventional '("feat"
+                               "fix"
+                               "bug"
+                               "issue"
+                               "sync"
+                               "merge"
+                               "alpha"
+                               "beta"
+                               "release"
+                               "refactor"
+                               "doc"))
 
 (defun delta-sync-string (module)
   (interactive "senter syncd module: ")
@@ -123,7 +134,6 @@
                   (magit-staged-files)) ))
 
     (string-join file-list ",") ))
-
 
 (defun delta-repo-dir ()
   (let
