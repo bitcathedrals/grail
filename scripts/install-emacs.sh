8d4a49c199d38b3ca92ddd50bb2ba2d6c1d2ac29
Michael Mattie <codermattie@runbox.com>
2024-05-17 21:41:56 -0700

    (feat): add arm64 brew install, compilation from source of emacs deps, and tweaking to the emacs build script [install-emacs.sh]

--- scripts/install-emacs.sh
+++ scripts/install-emacs.sh
@@ -3,7 +3,12 @@
 GIT=$HOME/code/emacs
 
 NATIVE=no
-SITTER=no
+SITTER=yes
+# ifavailable
+TLS=yes
+MODULES=yes
+MAIL=yes
+PNG=yes
 
 case $1 in
   "linux")
@@ -70,6 +75,16 @@ case $1 in
     cp desktop/emacs.desktop $LOCAL_DESKTOP/
     cp desktop/emacs-icon.png $HOME/tools/
   ;;
+  "macos-arm64")
+    test -d /opt/Homebrew || mkdir -p /opt/Homebrew
+    curl -L https://github.com/Homebrew/brew/tarball/master >/tmp/brew.xz
+    sudo tar xJf /tmp/brew.xz --strip 1 -C /opt/Homebrew/
+    sudo chown -R mattie /opt/Homebrew
+    ;;
+  "macos-deps")
+    eval "$(/opt/Homebrew/bin/brew shellenv)" && arch -arm64 brew reinstall --build-from-source pkg-config gnutls libpng tree-sitter little-cms2
+
+    ;;
    "macos-git")
     TOOLS=$HOME/tools/local/
 
@@ -104,7 +119,22 @@ case $1 in
       exit 1
     fi
 
-    if (cd $GIT && ./autogen.sh && ./configure --prefix=$TOOLS --with-native-compilation=$NATIVE --with-tree-sitter=$SITTER --with-xpm=no --with-gif=no --with-gnutls=ifavailable && make bootstrap)
+    if (cd $GIT && eval "$(/opt/Homebrew/bin/brew shellenv)" && \
+          arch -arm64 make extraclean && \
+          ./autogen.sh && \
+          arch -arm64 ./configure \
+                         --prefix=$TOOLS \
+                         --with-ns \
+                         --with-mailutils=$MAIL \
+                         --with-native-compilation=$NATIVE \
+                         --with-tree-sitter=$SITTER \
+                         --with-xpm=no \
+                         --with-png=$PNG \
+                         --with-gif=no \
+                         --with-gnutls=$TLS \
+                         --with-modules=$MODULES \
+                         --without-webp && \
+          arch -arm64 make bootstrap)
     then
       echo "compile ok."
     else
