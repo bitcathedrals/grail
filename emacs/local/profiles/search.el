6cc66b4d8b7782a64402699ce6a6cc4a3f1e23ab
Michael Mattie <codermattie@runbox.com>
2024-06-02 16:44:38 -0700

    (fix): fix the bindings for search [search.el]

--- emacs/local/profiles/search.el
+++ emacs/local/profiles/search.el
@@ -2,12 +2,12 @@
 (require 'visual-regexp-steroids)
 
 (custom-key-group "search" "s" t
-      ("g" . grep)
-      ("r" . rgrep)
-      ("o" . occur)
-      ("f" . vr/isearch-forward)
-      ("b" . vr/backward)
-       "t" . vr/query-replace)
+  ("g" . grep)
+  ("r" . rgrep)
+  ("o" . occur)
+  ("f" . vr/isearch-forward)
+  ("b" . vr/isearch-backward)
+  ("t" . vr/query-replace))
 
 (provide 'profile/search)
 
