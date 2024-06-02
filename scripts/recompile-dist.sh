9fa735f2f205ed52fd0eace39908fc48e742a1b2
Michael Mattie <codermattie@runbox.com>
2024-06-02 15:59:14 -0700

    (feat): add a script to purge and then compile the bytecode in emacs/dist/git

---
+++ scripts/recompile-dist.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+if [[ ! -d emacs ]]
+then
+  echo "recompile-dist.sh: need to be in the root of the grail repository"
+  exit 1
+fi
+
+find emacs/dist/git -name '*.elc' -print | xargs rm
+
+emacsclient -e '(compile-dist)'
