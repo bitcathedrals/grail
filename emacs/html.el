6ccce767d81d9d62f84eab50357be25eeb871d68
Michael Mattie <codermattie@runbox.com>
2024-07-01 12:48:24 -0700

    (fix): first attempt at html completion using company, not working yet [html.el]

--- emacs/html.el
+++ emacs/html.el
@@ -1,8 +1,20 @@
 (require 'sgml-mode)
 
+(defconst html-mode-config/name "html")
+
 (setq auto-mode-alist (append
                         (if (treesit-language-available-p 'html)
                           '(("\\.html\\'" . html-ts-mode))
                           '(("\\.html\\'" . html-mode)))
                         auto-mode-alist))
 
+(defun html-mode/configuration ()
+  (company-mode)
+  (setq company-backends (cons 'company-capf company-backends))
+
+  (programming-mode-generic 'html nil html-mode-config/name)
+
+  (dwim-tab-localize-context
+    (dwim-tab-make-expander 'dwim-tab/after-word 'company-complete)) )
+
+(add-hook 'html-mode-hook 'html-mode/configuration)
