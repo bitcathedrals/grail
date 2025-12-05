c59a57e37e4a013105f2835a768d852fb96ac6ad
Michael Mattie <codermattie@runbox.com>
2025-12-05 12:38:53 -0700

    (fix): fix the mode line hacking to show labels by using a insert past the first element with the descriptive label

--- emacs/local/elisp/grail-diff.el
+++ emacs/local/elisp/grail-diff.el
@@ -10,35 +10,34 @@
     (kill-buffer buf)
     (delete-frame frame)) )
 
+(defun insert-ediff-label-into-modeline (label)
+  (let
+    ((first-elm (car mode-line-format))
+     (rest-elm  (cdr mode-line-format)))
+
+    (setq mode-line-format (append first-elm (list label) rest-elm))
+    (print mode-line-format))
+  (force-mode-line-update))
+
 (defun grail-configure-ediff-change-window-names ()
   "grail-configure-ediff-change-window-names
 
    re-label the ediff windows
   "
-  ;; ediff has a function to strip the modeline if the first member of modeline
-  ;; is not one of the prefixes. if this becomes a problem insert our label
-  ;; after.
-
   (when ediff-buffer-A
     (with-current-buffer ediff-buffer-A
       (when (not (boundp 'grail-diff-A-modeline))
-        (setq mode-line-format (cons "local:" mode-line-format))
-        (set (make-local-variable 'grail-diff-A-modeline) t)
-        (force-mode-line-update)) ))
+        (insert-ediff-label-into-modeline "{local}") )) )
 
   (when ediff-buffer-B
     (with-current-buffer ediff-buffer-B
       (when (not (boundp 'grail-diff-B-modeline))
-        (setq ediff-diff-status (append "upstream:" ediff-diff-status))
-        (set (make-local-variable 'grail-diff-B-modeline) t)
-        (force-mode-line-update)) ))
+        (insert-ediff-label-into-modeline "{upstream}") )) )
 
   (when ediff-buffer-C
     (with-current-buffer ediff-buffer-C
       (when (not (boundp 'grail-diff-C-modeline))
-        (setq ediff-diff-status (append "merge:" ediff-diff-status))
-        (set (make-local-variable 'grail-diff-C-modeline) t)
-        (force-mode-line-update)) )) )
+        (insert-ediff-label-into-modeline "{merge}") )) ) )
 
 (defun grail-configure-ediff ()
   "configure-ediff
