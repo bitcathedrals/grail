05a3a38e1e878fbd39e46edf98128ef1446aa438
Michael Mattie <codermattie@runbox.com>
2025-12-09 11:03:51 -0700

    (fix): put magit and git tweaking into a git.el profile. This way it doesnt break all of programming. load and configure order on group 3 seems to have fixed magit issues

--- emacs/programming.el
+++ emacs/programming.el
@@ -46,40 +46,9 @@
 
 ;; advanced functionality
 
-(use-grail-profiles 3 "slime")
-
-;;
-;; magit
-;;
-
-(require 'magit)
+(use-grail-profiles 3 "slime" "git")
 
 (setq vc-follow-symlinks t)
-
-;; refresh after edit
-(add-hook 'after-save-hook 'magit-after-save-refresh-status t)
-
-(setq git-commit-style-convention-checks
-      (remove 'non-empty-second-line git-commit-style-convention-checks))
-
-(custom-key-group "magit git" "v" t
-  ("v" . magit-status)
-  ("l" . magit-log)
-
-  ("e" . magit-ediff-dwim)
-
-  ("a" . magit-stage)
-  ("u" . magit-unstage)
-  ("c" . magit-commit)
-  ("&" . magit-commit-squash)
-  ("x" . magit-commit-amend)
-
-  ("+" . magit-ediff-show-staged)
-  ("*" . magit-ediff-show-unstaged)
-  ("s" . magit-ediff-show-stash)
-
-  ("r" . magit-ediff-resolve-all)
-  ("p" . magit-push))
 
 ;;
 ;; C/C++
