952b16ae26bb4b5aa686a9a87be370509cbef5d1
Michael Mattie <codermattie@runbox.com>
2025-12-28 02:29:57 -0700

    (fix): correctly delete terminal buffer after the process dies

--- emacs/terminal.el
+++ emacs/terminal.el
@@ -15,10 +15,12 @@
 ;;                 IPC shell:  comint/term mode
 ;;----------------------------------------------------------------------
 
-(defadvice term (after terminal/after-hook)
-  (proc-close-on-exit/window))
+(defadvice term-handle-exit (after terminal-profile/handle-exit)
+  "Kills the current buffer when the term process exits."
+  (message "process exited. killing buffer.")
+  (kill-buffer (current-buffer)))
 
-(ad-activate 'term)
+(ad-activate 'term-handle-exit)
 
 (setq explicit-shell-file-name "zsh"
       explicit-zsh-args '("-i"))
