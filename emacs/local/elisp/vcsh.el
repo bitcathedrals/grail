f7105465c5efa36c9cd692049038ccc22436bfd0
Michael Mattie <codermattie@runbox.com>
2026-02-27 21:08:07 -0800

    (fix): start the rename process from pysh -> vc

--- emacs/local/elisp/vcsh.el
+++ emacs/local/elisp/vcsh.el
@@ -3,7 +3,7 @@
 (require 'subr-x)
 (require 'vc)
 
-(defun get-clean-pysh-buffer ()
+(defun get-clean-cvsh-buffer ()
   (let
     ((py-buffer (get-buffer-create "*py.sh output*")))
 
@@ -11,7 +11,7 @@
       (erase-buffer)
       py-buffer) ))
 
-(defun get-pysh-buffer ()
+(defun get-cvsh-buffer ()
   (get-buffer-create "*py.sh output*"))
 
 ;; repl    = execute ptpython in pyenv
@@ -28,7 +28,7 @@
 
 ;; start      = initiate an EDITOR session to update VERSION in python.sh, reload config,
 
-(defconst pysh-commands (sort
+(defconst cvsh-commands (sort
                           '("tools-unix"
                              "tools-zshrc"
                              "tools-custom"
@@ -108,7 +108,7 @@
                              "start")
                           'string-lessp))
 
-(defconst pysh-with-arguments
+(defconst cvsh-with-arguments
   '("global-virtual"
     "simple"
     "modadd"
@@ -118,7 +118,7 @@
     "beta"
     "start"))
 
-(defconst pysh-argument-prompts
+(defconst cvsh-argument-prompts
   '(("global-virtual" . "NAME,PYTHON_VERSION")
     ("simple"    . "PKG")
     ("modadd"    . "REPO,BRANCH,LOCALDIR")
@@ -128,15 +128,15 @@
     ("beta"      . "FEAT,MSG")
     ("start"     . "VERSION") ))
 
-(defun pysh-args-for (command)
+(defun cvsh-args-for (command)
   (let
-    ((prompt (assoc command pysh-argument-prompts)))
+    ((prompt (assoc command cvsh-argument-prompts)))
 
     (if prompt
       (cdr prompt)
       nil) ))
 
-(defun pysh-repo-dir ()
+(defun cvsh-repo-dir ()
   (let
     ((directory (vc-root-dir)))
 
@@ -150,53 +150,53 @@
             (if (equal major-mode 'dired-mode)
               (dired-current-directory)
               buffer-file-name))
-          (message "pysh: could not find a VC directory. exiting.")) )) ))
+          (message "cvsh: could not find a VC directory. exiting.")) )) ))
 
 
-(defun pysh-args (command-name)
-  (if (member command-name pysh-with-arguments)
+(defun cvsh-args (command-name)
+  (if (member command-name cvsh-with-arguments)
     (let
-      ((args (read-from-minibuffer (concat "pysh args [" (pysh-args-for command-name) "]: "))))
+      ((args (read-from-minibuffer (concat "cvsh args [" (cvsh-args-for command-name) "]: "))))
 
       (if args
         (cons command-name (split-string args))
-        (error (concat "pysh: no args given for command with args: " command-name))) )
+        (error (concat "cvsh: no args given for command with args: " command-name))) )
     (list command-name)) )
 
-(defun pysh-quit ()
+(defun cvsh-quit ()
   (interactive)
 
   (other-window 1)
   (delete-other-windows)
 
-  (kill-buffer (get-pysh-buffer)) )
+  (kill-buffer (get-cvsh-buffer)) )
 
-(defun pysh ()
+(defun cvsh ()
   (interactive)
   (let
     ((command (helm
                 :sources (helm-build-sync-source
                            "commands"
-                           :candidates pysh-commands
+                           :candidates cvsh-commands
                            :fuzzy-match t)
                 :preselect "info"
                 :buffer "py.sh commands")))
 
     (let*
-      ((default-directory (pysh-repo-dir))
+      ((default-directory (cvsh-repo-dir))
         (status (apply 'call-process
                   "py.sh"                             ;; program
                   nil                                 ;; infile
-                  (get-clean-pysh-buffer)             ;; output buffer
+                  (get-clean-cvsh-buffer)             ;; output buffer
                   nil                                 ;; don't display
-                  (pysh-args command)) ))             ;; pysh command and sometimes args
+                  (cvsh-args command)) ))             ;; cvsh command and sometimes args
 
       (if (equal status 0)
         (progn
-          (with-current-buffer (get-pysh-buffer)
-            (keymap-local-set "q" 'pysh-quit))
+          (with-current-buffer (get-cvsh-buffer)
+            (keymap-local-set "q" 'cvsh-quit))
 
-          (pop-to-buffer (get-pysh-buffer)) )
+          (pop-to-buffer (get-cvsh-buffer)) )
         (message "py.sh failed with: %d" status)) ) ))
 
-(provide 'pysh)
+(provide 'cvsh)
