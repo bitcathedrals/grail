e344f398b47bce60b11912ebf93e7ac38bb9cc3d
Michael Mattie <codermattie@runbox.com>
2026-04-21 13:53:33 -0700

    (dev): rework the data from old vc commands to a new set of commands

--- emacs/local/elisp/vcsh.el
+++ emacs/local/elisp/vcsh.el
@@ -14,127 +14,98 @@
 (defun get-cvsh-buffer ()
   (get-buffer-create "*vc output*"))
 
-;; repl    = execute ptpython in pyenv
-;; global-virtual
-;; simple     = <pkg> do a simple pyenv pip install without pipenv
-
-;; modadd <1> <2> <3>  = add a submodule where 1=repo 2=branch 3=localDir (commit after)
-;; modupdate <module>  = pull the latest version of the module
-;; modrm  <submodule>  = delete a submodule
-
-;; track <1> <2>  = set upstream tracking 1=remote 2=branch
-;; tag-alpha  <feat> <msg> = create an alpha tag with the feature branch name and message
-;; tag-beta   <feat> <msg> = create a beta tag with the devel branch feature and message
-
-;; start      = initiate an EDITOR session to update VERSION in python.sh, reload config,
-
 (defconst cvsh-commands (sort
-                          '("tools-unix"
-                             "tools-zshrc"
-                             "tools-custom"
-                             "tools-prompt"
+                          '("tools-zshrc"
+                            "tools-custom"
+                            "tools-prompt"
 
-                             "python-versions"
-                             "project-virtual"
-                             "global-virtual"
-                             "virtual-destroy"
-                             "project-destroy"
-                             "global-destroy"
-                             "virtual-list"
-                             "virtual-current"
+                            "tools-brew-init"
+                            "tools-brew-upgrade"
+                            "tools-brew-install"
+                            "tools-brew-rebuild"
 
-                             "minimal"
-                             "bootstrap"
-                             "pipfile"
-                             "project"
-                             "show-paths"
-                             "add-paths"
-                             "rm-paths"
-                             "site"
-                             "test"
-                             "python"
-                             "run"
+                            "dependencies-init"
+                            "dependencies-upgrade"
+                            "dependencies-install"
+                            "dependencies-rebuild"
 
-                             "versions"
-                             "locked"
-                             "all"
-                             "update"
-                             "remove"
-                             "list"
+                            "modadd"
+                            "modinit"
+                            "modpull"
+                            "modrm"
 
-                             "build"
-                             "buildset"
-                             "mkrelease"
-                             "runner"
+                            "begin"
+                            "end"
+                            "bug"
+                            "close"
 
-                             "modinit"
-                             "modall"
+                            "goto"
+                            "beta"
+                            "tag"
+                            "diff"
 
-                             "info"
-                             "verify"
-                             "status"
-                             "fetch"
-                             "pull"
-                             "staged"
-                             "merges"
-                             "releases"
-                             "history"
-                             "summary"
-                             "delta"
-                             "ahead"
-                             "behind"
-                             "release-report"
-                             "status-report"
+                            "report"
+                            "status"
 
-                             "graph"
-                             "upstream"
-                             "sync"
-                             "check"
+                            "goto"
 
-                             "release"
-                             "upload"
+                            "verify"
 
-                             ;; interactive commands
+                            "commit"
+                            "show"
+                            "get"
+                            "rebase"
+                            "patch"
 
-                             "global-virtual"
-                             "simple"
-                             "modadd"
-                             "modupdate"
+                            "pending"
+                            "list"
 
-                             "track"
-                             "alpha"
-                             "beta"
+                            "history"
+                            "ahead"
+                            "behind"
 
-                             "start")
-                          'string-lessp))
+                            "up"
+                            "down"
+                            "merge"
+                            "integrate"
+                            "publish"
+                            "cat"
+                            "rb"
 
-(defconst cvsh-with-arguments
-  '("global-virtual"
-    "simple"
-    "modadd"
-    "modupdate"
-    "track"
-    "alpha"
-    "beta"
-    "start"))
+                            "check"
+                            "start"
+                            "release"
+
+                            "help"
+                            "man")))
 
 (defconst cvsh-argument-prompts
-  '(("global-virtual" . "NAME,PYTHON_VERSION")
-    ("simple"    . "PKG")
-    ("modadd"    . "REPO,BRANCH,LOCALDIR")
-    ("modupdate" . "MODULE")
-    ("track"     . "REMOTE,BRANCH")
-    ("alpha"      . "FEAT,MSG")
-    ("beta"      . "FEAT,MSG")
-    ("start"     . "VERSION") ))
+  '(("commit"    . "LINE, LINE")
+    ("tools-brew-install" . "PACKAGES")
+    ("dependencies-install" . "PACKAGES")
 
-(defun cvsh-args-for (command)
-  (let
-    ((prompt (assoc command cvsh-argument-prompts)))
+    ("bug" . "NAME")
+    ("begin" . "NAME")
 
-    (if prompt
-      (cdr prompt)
-      nil) ))
+    ("bug" . "NAME")
+    ("begin" . "NAME")
+
+    ("goto" . "BRANCH")
+
+    ("beta" . "DESCRIPTION")
+    ("tag" . "VERSION, DESCRIPTION")
+
+    ("diff" . "OLDER_RANGE, NEWER_RANGE")
+    ("TAG" . "VERSION, DESCRIPTION")
+       ))
+
+    (defun cvsh-args-for (command)
+        (let
+            ((prompt (assoc command cvsh-argument-prompts)))
+
+            (if prompt
+                (cdr prompt)
+                nil) ))
 
 (defun cvsh-repo-dir ()
   (let
