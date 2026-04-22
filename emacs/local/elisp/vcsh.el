f3f512ac63bacdadeb0a25452f7d53132c3a6f78
Michael Mattie <codermattie@runbox.com>
2026-04-21 20:47:59 -0700

    (dev): reformat vcsh.el

--- emacs/local/elisp/vcsh.el
+++ emacs/local/elisp/vcsh.el
@@ -4,170 +4,170 @@
 (require 'vc)
 
 (defun get-clean-cvsh-buffer ()
-  (let
-    ((py-buffer (get-buffer-create "*vc output*")))
+    (let
+        ((py-buffer (get-buffer-create "*vc output*")))
 
-    (with-current-buffer py-buffer
-      (erase-buffer)
-      py-buffer) ))
+        (with-current-buffer py-buffer
+            (erase-buffer)
+            py-buffer) ))
 
 (defun get-cvsh-buffer ()
-  (get-buffer-create "*vc output*"))
+    (get-buffer-create "*vc output*"))
 
 (defconst cvsh-commands (sort
-                          '("tools-zshrc"
-                            "tools-custom"
-                            "tools-prompt"
+                            '("tools-zshrc"
+                                 "tools-custom"
+                                 "tools-prompt"
 
-                            "tools-brew-init"
-                            "tools-brew-upgrade"
-                            "tools-brew-install"
-                            "tools-brew-rebuild"
+                                 "tools-brew-init"
+                                 "tools-brew-upgrade"
+                                 "tools-brew-install"
+                                 "tools-brew-rebuild"
 
-                            "dependencies-init"
-                            "dependencies-upgrade"
-                            "dependencies-install"
-                            "dependencies-rebuild"
+                                 "dependencies-init"
+                                 "dependencies-upgrade"
+                                 "dependencies-install"
+                                 "dependencies-rebuild"
 
-                            "modadd"
-                            "modinit"
-                            "modpull"
-                            "modrm"
+                                 "modadd"
+                                 "modinit"
+                                 "modpull"
+                                 "modrm"
 
-                            "begin"
-                            "end"
-                            "bug"
-                            "close"
+                                 "begin"
+                                 "end"
+                                 "bug"
+                                 "close"
 
-                            "goto"
-                            "beta"
-                            "tag"
-                            "diff"
+                                 "goto"
+                                 "beta"
+                                 "tag"
+                                 "diff"
 
-                            "report"
-                            "status"
+                                 "report"
+                                 "status"
 
-                            "goto"
+                                 "goto"
 
-                            "verify"
+                                 "verify"
 
-                            "commit"
-                            "show"
-                            "get"
-                            "rebase"
-                            "patch"
+                                 "commit"
+                                 "show"
+                                 "get"
+                                 "rebase"
+                                 "patch"
 
-                            "pending"
-                            "list"
+                                 "pending"
+                                 "list"
 
-                            "history"
-                            "ahead"
-                            "behind"
+                                 "history"
+                                 "ahead"
+                                 "behind"
 
-                            "up"
-                            "down"
-                            "merge"
-                            "integrate"
-                            "publish"
-                            "cat"
-                            "rb"
+                                 "up"
+                                 "down"
+                                 "merge"
+                                 "integrate"
+                                 "publish"
+                                 "cat"
+                                 "rb"
 
-                            "check"
-                            "start"
-                            "release"
+                                 "check"
+                                 "start"
+                                 "release"
 
-                            "help"
-                            "man")))
+                                 "help"
+                                 "man")))
 
 (defconst cvsh-argument-prompts
-  '(("commit"    . "LINE, LINE")
-    ("tools-brew-install" . "PACKAGES")
-    ("dependencies-install" . "PACKAGES")
+    '(("commit"    . "LINE, LINE")
+         ("tools-brew-install" . "PACKAGES")
+         ("dependencies-install" . "PACKAGES")
 
-    ("bug" . "NAME")
-    ("begin" . "NAME")
+         ("bug" . "NAME")
+         ("begin" . "NAME")
 
-    ("bug" . "NAME")
-    ("begin" . "NAME")
+         ("bug" . "NAME")
+         ("begin" . "NAME")
 
-    ("goto" . "BRANCH")
+         ("goto" . "BRANCH")
 
-    ("beta" . "DESCRIPTION")
-    ("tag" . "VERSION, DESCRIPTION")
+         ("beta" . "DESCRIPTION")
+         ("tag" . "VERSION, DESCRIPTION")
 
-    ("diff" . "OLDER_RANGE, NEWER_RANGE")
-    ("TAG" . "VERSION, DESCRIPTION")
-       ))
+         ("diff" . "OLDER_RANGE, NEWER_RANGE")
+         ("TAG" . "VERSION, DESCRIPTION")
+         ))
 
-    (defun cvsh-args-for (command)
-        (let
-            ((prompt (assoc command cvsh-argument-prompts)))
+(defun cvsh-args-for (command)
+    (let
+        ((prompt (assoc command cvsh-argument-prompts)))
 
-            (if prompt
-                (cdr prompt)
-                nil) ))
+        (if prompt
+            (cdr prompt)
+            nil) ))
 
 (defun cvsh-repo-dir ()
-  (let
-    ((directory (vc-root-dir)))
+    (let
+        ((directory (vc-root-dir)))
 
-    (if directory
-      directory
-      (let
-        ((found (call-interactively 'helm-find-files)))
+        (if directory
+            directory
+            (let
+                ((found (call-interactively 'helm-find-files)))
 
-        (if found
-          (with-current-buffer found
-            (if (equal major-mode 'dired-mode)
-              (dired-current-directory)
-              buffer-file-name))
-          (message "cvsh: could not find a VC directory. exiting.")) )) ))
+                (if found
+                    (with-current-buffer found
+                        (if (equal major-mode 'dired-mode)
+                            (dired-current-directory)
+                            buffer-file-name))
+                    (message "cvsh: could not find a VC directory. exiting.")) )) ))
 
 
 (defun cvsh-args (command-name)
-  (if (member command-name cvsh-with-arguments)
-    (let
-      ((args (read-from-minibuffer (concat "cvsh args [" (cvsh-args-for command-name) "]: "))))
+    (if (member command-name cvsh-with-arguments)
+        (let
+            ((args (read-from-minibuffer (concat "cvsh args [" (cvsh-args-for command-name) "]: "))))
 
-      (if args
-        (cons command-name (split-string args))
-        (error (concat "cvsh: no args given for command with args: " command-name))) )
-    (list command-name)) )
+            (if args
+                (cons command-name (split-string args))
+                (error (concat "cvsh: no args given for command with args: " command-name))) )
+        (list command-name)) )
 
 (defun cvsh-quit ()
-  (interactive)
+    (interactive)
 
-  (other-window 1)
-  (delete-other-windows)
+    (other-window 1)
+    (delete-other-windows)
 
-  (kill-buffer (get-cvsh-buffer)) )
+    (kill-buffer (get-cvsh-buffer)) )
 
 (defun vcsh ()
-  (interactive)
-  (let
-    ((command (helm
-                :sources (helm-build-sync-source
-                           "commands"
-                           :candidates cvsh-commands
-                           :fuzzy-match t)
-                :preselect "info"
-                :buffer "vc commands")))
+    (interactive)
+    (let
+        ((command (helm
+                      :sources (helm-build-sync-source
+                                   "commands"
+                                   :candidates cvsh-commands
+                                   :fuzzy-match t)
+                      :preselect "info"
+                      :buffer "vc commands")))
 
-    (let*
-      ((default-directory (cvsh-repo-dir))
-        (status (apply 'call-process
-                  "vc"                             ;; program
-                  nil                                 ;; infile
-                  (get-clean-cvsh-buffer)             ;; output buffer
-                  nil                                 ;; don't display
-                  (cvsh-args command)) ))             ;; cvsh command and sometimes args
+        (let*
+            ((default-directory (cvsh-repo-dir))
+                (status (apply 'call-process
+                            "vc"                             ;; program
+                            nil                                 ;; infile
+                            (get-clean-cvsh-buffer)             ;; output buffer
+                            nil                                 ;; don't display
+                            (cvsh-args command)) ))             ;; cvsh command and sometimes args
 
-      (if (equal status 0)
-        (progn
-          (with-current-buffer (get-cvsh-buffer)
-            (keymap-local-set "q" 'cvsh-quit))
+            (if (equal status 0)
+                (progn
+                    (with-current-buffer (get-cvsh-buffer)
+                        (keymap-local-set "q" 'cvsh-quit))
 
-          (pop-to-buffer (get-cvsh-buffer)) )
-        (message "vc failed with: %d" status)) ) ))
+                    (pop-to-buffer (get-cvsh-buffer)) )
+                (message "vc failed with: %d" status)) ) ))
 
 (provide 'vcsh)
